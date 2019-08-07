109a89de3387fffdcbd2cec20586877ba70f2128
yaoshunya <newton800pola@gmail.com>
2019-08-07 16:56:28 +0900

    can get gradients

--- darkflow/net/yolov2/train.py
+++ darkflow/net/yolov2/train.py
@@ -7,6 +7,7 @@ import os
 import math
 import pdb
 import cv2
+from tensorflow.python.ops import tensor_array_ops
 
 def mask_anchor(anchor,H):
     img_x = 1000
@@ -58,14 +59,14 @@ def mask_anchor(anchor,H):
                     mask = resize_mask[np.newaxis]
                 else:
                     mask = np.append(mask,resize_mask[np.newaxis],axis=0)
-                print(l)
+
 
             step_x += step_size
             if t == 0:
                 mask_ = mask[np.newaxis]
             else:
                 mask_ = np.append(mask_,mask[np.newaxis],axis=0)
-
+        print(i)
         step_y += step_size
         if i == 0:
             mask_fi = mask_[np.newaxis]
@@ -83,155 +84,25 @@ def shift_x_y(coords,H,W,B,image):
     image = tf.reshape(tf.convert_to_tensor(image,dtype=tf.float32),(H*W,B,H,W))
     n_fc = 6
 
-    initial = np.array([[1., 0, 0], [0, 1., 0]])
-    initial = initial.astype('float32').flatten()
-    W_fc1 = tf.Variable(tf.zeros([H*W*(H*W), n_fc]), name='W_fc1')
-    b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
-    h_fc1 = tf.matmul(tf.zeros([B, H*W*(H*W)]), W_fc1) + b_fc1
     h_trans = spatial_transformer_network(image, coords)
-"""
-    init_HW=(0,image,shift_image)
-    shift_image = tf.while_loop(cond=condition_HW,body=body_HW,loop_vars=init_HW)
-def condition_HW(i,image,shift_image):
-    return i < 361
-def body_HW(i,image,shift_image):
-    init_B = (i,0,image,shift_image)
-    return tf.while_loop(cond=condition_B,body=body_B,loop_vars=init_B)
-def condition_B(i,k,image,shift_image):
-    return k < 5
-def body_B(i,k,image,shift_image):
-    img_B = image[i][k]
-    x = tf.where(tf.equal(img_B,255))
-    return 0
 
-def condition(zero_matrix):
-    return tf.Variable(zero_matrix)
-
-def shift_x_y_(coords,H,W,B,image):
-    x_shift = tf.reshape(coords[:,:,:,0],[-1,H,W,B])#<tf.Tensor 'Reshape_2:0' shape=(?, 19, 19, 5) dtype=float32>
-    y_shift = tf.reshape(coords[:,:,:,1],[-1,H,W,B])
-    theta = tf.reshape(coords[:,:,:,2],[-1,H,W,B])
-
-    for h in range(H):
-        for w in range(W):
-            x_ = x_shift[:,h,w,:] #<tf.Tensor 'strided_slice_4:0' shape=(?, 5) dtype=float32>
-            y_ = y_shift[:,h,w,:] #<tf.Tensor 'strided_slice_5:0' shape=(?, 5) dtype=float32>
-            theta_ = theta[:,h,w,:] #<tf.Tensor 'strided_slice_6:0' shape=(?, 5) dtype=float32>
-            new_x = tf.math.cos(theta_)*h-tf.math.sin(theta_)*h+x_ #<tf.Tensor 'add:0' shape=(?, 5) dtype=float32>
-            new_y = tf.math.sin(theta_)*w-tf.math.cos(theta_)*w+y_ #<tf.Tensor 'add_1:0' shape=(?, 5) dtype=float32>
-            for k in range(B):
-                x = tf.cast(tf.transpose(new_x)[k],tf.float32)
-                y = tf.cast(tf.transpose(new_y)[k],tf.float32)
-                imgs = image[h][w][k]
-                im = my_img_translate(imgs, x,y)
-                #pdb.set_trace()
-                im_gra = im[1][1]
-                im = im[0]
-                #pdb.set_trace()
-                im = tf.cast(im,tf.int32)
-                im = tf.expand_dims(tf.reshape(im,[H,W]),0)
-
-                im_gra = tf.expand_dims(im_gra,0)
-                if k == 0:
-                    im_k = im
-                    im_gra_k = im_gra
-                else:
-                    #pdb.set_trace()
-                    im_k = tf.concat([im_k,im],0)
-                    im_gra_k = tf.concat([im_gra_k,im_gra],0)
-            #pdb.set_trace()
-            im_gra_k = tf.expand_dims(im_gra_k,0)
-            im_k = tf.expand_dims(im_k,0)
-            if h==0 and w==0:
-                im_w=im_k
-                im_gra_w=im_gra_k
-            else:
-                im_w=tf.concat([im_w,im_k],0)
-                im_gra_w=tf.concat([im_gra_w,im_gra_k],0)
-                print(h)
-
-    return im_w,im_gra_w
-
+    return h_trans
 
 #https://stackoverflow.com/questions/42252040/how-to-translateor-shift-images-in-tensorflow
 #https://zhengtq.github.io/2018/12/20/tf-tur-perspective-transform/
 
-def my_img_translate(imgs, x,y):
-    # Interpolation model has to be fixed due to limitations of tf.custom_gradient
-    interpolation = 'NEAREST'
-    imgs = imgs[np.newaxis,:,:,np.newaxis]
-    imgs = tf.convert_to_tensor(imgs,dtype=tf.float32)
-    t = tf.placeholder(tf.float32,shape=[None,None,None,None])
-    imgs = t+imgs
-    x=tf.expand_dims(x,1)
-    y=tf.expand_dims(y,1)
-    translates = tf.concat([x,y],1)
-
-    imgs_translated = tf.contrib.image.translate(imgs, translates, interpolation=interpolation)
-
-    def grad(img_translated_grads):
-
-        translates_x = translates[:, 0] #<tf.Tensor 'gradients/IdentityN_grad/strided_slice:0' shape=(?,) dtype=float32>
-        translates_y = translates[:, 1] #<tf.Tensor 'gradients/IdentityN_grad/strided_slice_1:0' shape=(?,) dtype=float32>
-        translates_zero = tf.zeros_like(translates_x) #<tf.Tensor 'gradients/IdentityN_grad/zeros_like:0' shape=(?,) dtype=float32>
-        # X gradients
-        imgs_x_grad = (imgs[:, :, :-2] - imgs[:, :, 2:]) / 2 #<tf.Tensor 'gradients/IdentityN_grad/truediv:0' shape=(?, ?, ?, ?) dtype=float32>
-        imgs_x_grad = tf.concat([(imgs[:, :, :1] - imgs[:, :, 1:2]),
-                                 imgs_x_grad,
-                                 (imgs[:, :, -2:-1] - imgs[:, :, -1:])], axis=2) #<tf.Tensor 'gradients/IdentityN_grad/truediv:0' shape=(?, ?, ?, ?) dtype=float32>
-        imgs_x_grad_translated = tf.contrib.image.translate(
-            imgs_x_grad, tf.stack([translates_x, translates_zero], axis=1),
-            interpolation=interpolation)
-        translates_x_grad = tf.reduce_sum(img_translated_grads * imgs_x_grad_translated, axis=(1, 2, 3)) #
-        # Y gradients
-        imgs_y_grad = (imgs[:, :-2] - imgs[:, 2:]) / 2
-        imgs_y_grad = tf.concat([(imgs[:, :1] - imgs[:, 1:2]),
-                                 imgs_y_grad,
-                                 (imgs[:, -2:-1] - imgs[:, -1:])], axis=1)
-        imgs_y_grad_translated = tf.contrib.image.translate(
-            imgs_y_grad, tf.stack([translates_zero, translates_y], axis=1),
-            interpolation=interpolation)
-        translates_y_grad = tf.reduce_sum(img_translated_grads * imgs_y_grad_translated, axis=(1, 2, 3))
-        # Complete gradient
-        translates_grad = tf.stack([translates_x_grad, translates_y_grad], axis=1) #<tf.Tensor 'gradients/IdentityN_grad/stack_2:0' shape=(?, 2) dtype=float32>
-
-        return None, translates_grad
-    gr = grad(imgs_translated)
-    return imgs_translated,gr
-"""
-"""
-def return_image_gra(imgs,H,W):
-    for i in range(H*W):
-
-        dy_parts,dx_parts = tf.image.image_gradients(tf.transpose(imgs,[1,0,2,3,4])[i])
-
-
-        dy_parts = tf.expand_dims(dy_parts,0)
-        dx_parts = tf.expand_dims(dx_parts,0)
-
-        if i == 0:
-            dy = dy_parts
-            dx = dx_parts
-        else:
-            dy = tf.concat([dy,dy_parts],0)
-            dx = tf.concat([dx,dx_parts],0)
-    return tf.transpose(dy,[1,0,2,3,4]),tf.transpose(dx,[1,0,2,3,4])
-"""
-def condition(i,N,theta,input_fmap):
+def condition(i,N,theta,input_fmap,l_):
     return i < N
 
-def update(i,N,theta,input_fmap):
+def update(i,N,theta,input_fmap,l_):
     batch_grids = affine_grid_generator(19, 19, theta[i])
 
     x_s = batch_grids[:, 0, :, :]
     y_s = batch_grids[:, 1, :, :]
-    #pdb.set_trace()
+
     out_fmap = bilinear_sampler(input_fmap, x_s, y_s)
-    pdb.set_trace()
-    return 0
-
-
-
+    l_ = l_.write(i,out_fmap)
+    return i+1,N,theta,tf.transpose(out_fmap,[0,3,1,2]),l_
 
 def spatial_transformer_network(input_fmap, theta, out_dims=None, **kwargs):
 
@@ -252,27 +123,13 @@ def spatial_transformer_network(input_fmap, theta, out_dims=None, **kwargs):
 
     # reshape theta to (B, 2, 3)
     #theta = tf.reshape(theta, [B, 2, 3])
-    init_val = (0,200,theta,input_fmap)
+    out_list = tensor_array_ops.TensorArray(tf.float32, size=1, dynamic_size=True)
+    init_val = (0,200,theta,input_fmap,out_list)
+    #pdb.set_trace()
+
     out_fmap = tf.while_loop(cond=condition,body=update,loop_vars=init_val)
-    return out_fmap
-"""
-    # generate grids of same size or upsample/downsample if specified
-    if out_dims:
-        out_H = out_dims[0]
-        out_W = out_dims[1]
-        batch_grids = affine_grid_generator(out_H, out_W, theta)
-    else:
-        batch_grids = affine_grid_generator(H, W, theta)
-
-
-    x_s = batch_grids[:, 0, :, :]
-    y_s = batch_grids[:, 1, :, :]
-
-    # sample input with grid to get output
-    out_fmap = bilinear_sampler(input_fmap, x_s, y_s)
-"""
-
-
+    out_stack = out_fmap[4].stack()
+    return out_stack
 
 def get_pixel_value(img, x, y):
     shape = tf.shape(x)
@@ -326,7 +183,8 @@ def affine_grid_generator(height, width, theta):
 
 
 def bilinear_sampler(img, x, y):
-    pdb.set_trace()
+    #pdb.set_trace()
+    img = tf.transpose(img,[0,2,3,1])
     H = tf.shape(img)[1]
     W = tf.shape(img)[2]
     max_y = tf.cast(H - 1, 'int32')
@@ -380,6 +238,14 @@ def bilinear_sampler(img, x, y):
 
     return out
 
+def normalize_(x):
+
+    x = tf.reshape(x,(200,361,361,5))
+    x_max = tf.tile(tf.expand_dims(tf.argmax(x,2),2),[1,1,361,1])
+    x_min = tf.tile(tf.expand_dims(tf.argmin(x,2),2),[1,1,361,1])
+    x = tf.math.divide(tf.subtract(tf.cast(x,tf.int64),x_min),tf.subtract(x_max,x_min))
+
+    return x
 
 def loss(self, net_out):
     """
@@ -432,20 +298,45 @@ def loss(self, net_out):
     coords = net_out_reshape[:, :, :, :, :3]
     coords = tf.reshape(coords, [-1, H*W, B, 3])
 
-    area_pred,gr = shift_x_y(coords,H,W,B,anchors)
+    area_pred = shift_x_y(coords,H,W,B,anchors)
 
-    area_pred = tf.transpose(area_pred,(0,2,3,1))
-    max = tf.cast(tf.tile(tf.expand_dims(tf.argmax(area_pred,3),3),[1,1,1,5]),tf.int32)
-    min = tf.cast(tf.tile(tf.expand_dims(tf.argmin(area_pred,3),3),[1,1,1,5]),tf.int32)
-    area_pred = tf.reshape(tf.math.divide(tf.math.subtract(area_pred,min),tf.math.subtract(max,min)),[-1,HW,H,W,B])
+    #max = tf.cast(tf.tile(tf.expand_dims(tf.argmax(area_pred,3),3),[1,1,1,5]),tf.int32)
+    #min = tf.cast(tf.tile(tf.expand_dims(tf.argmin(area_pred,3),3),[1,1,1,5]),tf.int32)
+    #area_pred = tf.reshape(tf.math.divide(tf.math.subtract(area_pred,min),tf.math.subtract(max,min)),[-1,HW,H,W,B])
     _areas = tf.transpose(_areas,(0,1,3,4,2))
+    #pdb.set_trace()
     intersect = tf.math.multiply(tf.cast(area_pred,tf.float64),tf.cast(_areas,tf.float64))
-    area_pred = tf.cast(area_pred,tf.float32)
-    intersect = tf.cast(intersect,tf.float32)
-    iou = tf.truediv(intersect, _areas + area_pred - intersect) #<tf.Tensor 'truediv_3611:0' shape=(?, 361, 19, 19, 5) dtype=float32>
 
+    intersect = tf.reduce_sum(tf.math.sign(tf.reshape(intersect,(200,361,361,5))),2)
+    area_pred = tf.reduce_sum(tf.math.sign(tf.reshape(area_pred,(200,361,361,5))),2)
+    _areas= tf.reduce_sum(tf.math.sign(tf.reshape(_areas,(200,361,361,5))),2)
+
+    #x_max = tf.tile(tf.expand_dims(tf.argmax(intersect,2),2),[1,1,361,1])
+    #x_min = tf.tile(tf.expand_dims(tf.argmin(intersect,2),2),[1,1,361,1])
+    #intersect = tf.math.divide(tf.subtract(tf.cast(intersect,tf.int64),x_min),tf.subtract(x_max,x_min))*255
+    #pdb.set_trace()
+    #intersect = tf.reduce_sum(x,2)
+    #_areas = tf.reduce_sum(normalize_(_areas),2)
+    #area_pred = tf.reduce_sum(normalize_(area_pred),2)
+
+
+    iou = tf.math.divide(intersect,tf.cast(_areas+area_pred,tf.float64)-intersect)
+
+    pdb.set_trace()
+
+    """
+    intersect_num = tf.math.count_nonzero(tf.reshape(intersect,(200,361,361,5)),2)
+    area_pred_num = tf.math.count_nonzero(tf.reshape(area_pred,(200,361,361,5)),2)
+    _areas_num = tf.math.count_nonzero(tf.reshape(_areas,(200,361,361,5)),2)
+    """
+    #iou = tf.truediv(intersect_num, _areas_num + area_pred_num - intersect_num) #<tf.Tensor 'truediv_3611:0' shape=(?, 361, 19, 19, 5) dtype=float32>
+    pdb.set_trace()
+    iou = tf.reshape(iou,[-1,H*W*B])
+    loss = tf.reduce_sum(iou,1)
 
     print('Building {} loss'.format(m['model']))
+
+    pdb.set_trace()
 
     self.loss = .5 * tf.reduce_mean(loss)
     tf.summary.scalar('{} loss'.format(m['model']), self.loss)
